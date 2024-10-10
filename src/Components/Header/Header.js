705b261031a2c1f066c660fd73325c9eeb3de180
leosoplapuco <leosoplapuco29@gmail.com>
2024-10-09 23:36:29 -0500

    Actualización 0.4

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,22 +4,54 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <header className="header">
-            <div className="logo">
-                <a href="/">Google Cloud</a>
-            </div>
-            <nav className="nav">
-                <ul>
-                    <li><a href="#products">Products</a></li>
-                    <li><a href="#solutions">Solutions</a></li>
-                    <li><a href="#pricing">Pricing</a></li>
-                    <li><a href="#docs">Documentation</a></li>
-                    <li><a href="#support">Support</a></li>
-                </ul>
-            </nav>
-            <div className="auth-buttons">
-                <a href="#sign-in" className="button">Sign in</a>
-                <a href="#get-started" className="button button-primary">Get started</a>
+        <header>
+            <div className='header'>
+                <div className='header-top-container'>
+                    <div className='header-top-content'>
+                        <a href='/' className='header-top-logo'>
+                            <h2>Grúas <b>Ememca</b></h2>
+                        </a>
+
+                        <nav className='header-menu-container'>
+                            <ul className='header-menu'>
+                                <li>
+                                    <a href='/' className='menu-link menu-link-1'>
+                                        <h2>Inicio</h2>
+                                    </a>
+                                </li>
+                                <li>
+                                    <button type='button' className='menu-link menu-link-2'>
+                                        <h2>Servicios</h2>
+                                        <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type='button' className='menu-link menu-link-3'>
+                                        <h2>Maquinaria</h2>
+                                        <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type='button' className='menu-link menu-link-4'>
+                                        <h2>Proyectos</h2>
+                                        <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type='button' className='menu-link menu-link-5'>
+                                        <h2>Nosotros</h2>
+                                        <span class="material-symbols-outlined">keyboard_arrow_down</span>
+                                    </button>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <a className='link link-2 margin-left'>
+                            <h2 className='link-text'>Contáctanos</h2>
+                            <span class="material-symbols-outlined">mail</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </header>
     );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import './HomePage.css';
 
 import Hero from '../../Components/Hero/Hero';
+import Header from '../../Components/Header/Header';
 
 function HomePage() {
     return (
         <>
+            <Header/>
             <Hero/>
         </>
     );
