25f29ddc7ab23a161ae89f0daf7ce5e41c447967
leosoplapuco <leosoplapuco@leosoplapuco.com>
2024-10-26 15:58:14 -0500

    Actualización 2.9

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 import HomePage from './pages/HomePage/HomePage';
 import Services from './pages/Services/Services';
@@ -6,21 +7,25 @@ import Machines from './pages/Machines/Machines';
 import Projects from './pages/Projects/Projects';
 import Us from './pages/Us/Us';
 import Blog from './pages/Blog/Blog';
+import Article from './pages/Blog/Article/Article';
 
 function App() {
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" Component={HomePage}/>
-                    <Route path="/servicios" Component={Services}/>
-                    <Route path="/maquinaria" Component={Machines}/>
-                    <Route path="/proyectos" Component={Projects}/>
-                    <Route path="/nosotros" Component={Us}/>
-                    <Route path="/blog" Component={Blog}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <HelmetProvider>
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" Component={HomePage} />
+                        <Route path="/servicios" Component={Services} />
+                        <Route path="/maquinaria" Component={Machines} />
+                        <Route path="/proyectos" Component={Projects} />
+                        <Route path="/nosotros" Component={Us} />
+                        <Route path="/blog" Component={Blog} />
+                        <Route path="/blog/:slug" Component={Article} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </HelmetProvider>
     );
 }
 
--- src/pages/Us/Us.js
+++ src/pages/Us/Us.js
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet-async';
+
 import './Us.css';
 
 import Header from '../../Components/Header/Header';
@@ -5,9 +7,29 @@ import Header from '../../Components/Header/Header';
 function Us() {
     return (
         <>
+
+            <Helmet>
+                <title>Servicios - Nombre de tu Empresa</title>
+                <meta name="description" content="Descripción de la página de servicios." />
+                <link rel="canonical" href="https://www.gruasememca.com/servicios" />
+
+                <meta property="og:title" content="Servicios - Nombre de tu Empresa" />
+                <meta property="og:description" content="Descripción de los servicios que ofrecemos." />
+                <meta property="og:image" content="URL-de-imagen-de-servicios.jpg" />
+                <meta property="og:url" content="https://www.gruasememca.com/servicios" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Servicios - Nombre de tu Empresa" />
+                <meta name="twitter:description" content="Descripción de los servicios que ofrecemos." />
+                <meta name="twitter:image" content="URL-de-imagen-de-servicios.jpg" />
+            </Helmet>
+
+            <Header/>
+
             <main>
-                <Header/>
+                <p>Hello</p>
             </main>
+
         </>
     );
 }
