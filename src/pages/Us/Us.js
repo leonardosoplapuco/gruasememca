6483b1e828867a15f28461f43efdb0fb3a513c51
leosoplapuco <leosoplapuco@leosoplapuco.com>
2024-11-02 14:37:55 -0500

    Actualización 4.0 - leonardosoplapuco

--- src/pages/Us/Us.js
+++ src/pages/Us/Us.js
@@ -1,30 +1,35 @@
 import { Helmet } from 'react-helmet-async';
 
+import { useTranslation } from 'react-i18next';
+
 import './Us.css';
 
-// import Header from '../../Components/Header/Header';
+import Header from '../../Components/Header/Header';
 
-function Us() {
+function Us(){
+
+    const { t } = useTranslation();
+
     return (
         <>
 
             <Helmet>
-                <title>Nosotros | Grúas Ememca SAC</title>
-                <meta name="description" content="Descripción de la página de Nosotros." />
-                <link rel="canonical" href="https://www.gruasememca.com/nosotros" />
+                <title>{t('Nosotros.Head.title')}</title>
+                <meta name="description" content={t('Nosotros.Head.meta-description')} />
+                <link rel="canonical" href={t('Nosotros.Head.link-canonical')} />
 
-                <meta property="og:title" content="Servicios - Nombre de tu Empresa" />
-                <meta property="og:description" content="Descripción de los servicios que ofrecemos." />
-                <meta property="og:image" content="URL-de-imagen-de-servicios.jpg" />
-                <meta property="og:url" content="https://www.gruasememca.com/nosotros" />
+                <meta property="og:title" content={t('Nosotros.Head.meta-og-title')} />
+                <meta property="og:description" content={t('Nosotros.Head.meta-og-description')} />
+                <meta property="og:image" content={t('Nosotros.Head.meta-og-image')} />
+                <meta property="og:url" content={t('Nosotros.Head.meta-og-url')} />
 
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content="Servicios - Nombre de tu Empresa" />
-                <meta name="twitter:description" content="Descripción de los servicios que ofrecemos." />
-                <meta name="twitter:image" content="URL-de-imagen-de-servicios.jpg" />
+                <meta name="twitter:card" content={t('Nosotros.Head.meta-twitter-card')} />
+                <meta name="twitter:title" content={t('Nosotros.Head.meta-twitter-title')} />
+                <meta name="twitter:description" content={t('Nosotros.Head.meta-twitter-description')} />
+                <meta name="twitter:image" content={t('Nosotros.Head.meta-twitter-image')} />
             </Helmet>
 
-            {/* <Header/> */}
+            <Header/>
 
             <main>
                 <p>Hello</p>
