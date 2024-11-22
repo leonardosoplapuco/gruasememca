87f87978fe342530d00c668d65da46298422f614
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2024-11-22 14:26:50 -0500

    Actualización 5.8 - leonardosoplapuco

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,45 +1,21 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
-function Header(){
-    const { t } = useTranslation('hero');
+import HeroVideo from './heroVideo'
+
+function Hero({title, titleSpan}){
     return (
-        <section className='hero-container'>
-            <div className='hero'>
-                <div className='hero-card hero-card-1 d-flex-column gap-10'>
-                    <h1>{t('h1.0.textOne')} <b>{t('h1.0.textTwo')}</b></h1>
+        <>
 
-                    <ul className='list-simple margin-bottom-20'>
-                        {t('list', { returnObjects: true }).map((item) => (
-                            <li key={item.id}>
-                                <span className="material-symbols-outlined list-icon-check">check</span>
-                                <p>{item.text}</p>
-                            </li>
-                        ))}
-                    </ul>
+            <HeroVideo/>
+        
+            <section className='hero-container'>
+                <div className='hero'>
+                    <h1>{title}<span>{titleSpan}</span></h1>
+                </div>
+            </section>
 
-                    <ul className='d-flex gap-10'>
-                        <li>
-                            <a href={t('links.0.href')} className='button-link button-link-2'>
-                                <span className="material-symbols-outlined">diversity_3</span>
-                                <p className='button-link-text'>{t('links.0.text')}</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href={t('links.1.href')} className='button-link button-link-1'>
-                                <span className="material-symbols-outlined">phone_in_talk</span>
-                                <p className='button-link-text'>{t('links.1.text')}</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className='hero-card hero-card-2'>
-                    <img src='https://www.gruasememca.com/media/img/gallery-2.webp' alt={t('cardTwo.0.alt')} className='img-simple'></img>
-                    <p className='text'>{t('cardTwo.0.text')}</p>
-                </div>
-            </div>
-        </section>
+        </>
     );
 }
 
-export default Header;
+export default Hero;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { useTranslation } from 'react-i18next';
 
-import Header from '../Components/Header';
+// import Header from '../Components/Header';
 import Hero from '../Components/Hero';
-import ChatBot from '../Components/ChatBot'
+// import ChatBot from '../Components/ChatBot'
 
 const Home = () => {
     const { t } = useTranslation('home');
@@ -27,93 +27,14 @@ const Home = () => {
                 <meta name="twitter:image" content={t('head.0.twitterImage')} />
             </Helmet>
 
-            <Header/>
+            {/* <Header/> */}
 
             <main>
-                <Hero/>
-                <ChatBot/>
-
-                <div className="block-container">
-                    <section className="block-content d-flex-column gap-20">
-                        <div className="home-companies">
-                            <ul>
-                                {t('homeCompanies', { returnObjects: true })[0]?.companies.map((company) => (
-                                    <li key={company.id}>
-                                        <img src={company.imgSrc} alt={company.imgAlt}></img>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-
-                        <a href={t('homeCompanies.1.href')} className="margin-left button-text-link button-text-link-1">
-                            <p className="button-link-text">{t('homeCompanies.1.text')}</p>
-                            <span className="material-symbols-outlined">chevron_right</span>
-                        </a>
-                    </section>
-                </div>
-
-                <div className="block-container">
-                    <section className="block-content d-flex-column gap-20">
-                        <div className="block-title-container">
-                            <h3 className="block-title">{t('homeServices.0.title')}</h3>
-                            <p className="text">{t('homeServices.0.textIntro')}</p>
-                        </div>
-
-                        <div className="index-services d-grid-2-1 gap-20">
-                            <div className="index-services-target index-services-target-1">
-                                <ul className="faq">
-                                    {t("homeServicesFaq", { returnObjects: true }).map((faq) => (
-                                        <li key={faq.id} className={`faq-target faq-target-${faq.id}`}>
-                                            <div className="faq-target-top">
-                                                <p className="faq-target-title">{faq.title}</p>
-                                                <span className="material-symbols-outlined">keyboard_arrow_down</span>
-                                            </div>
-                                            <div className="faq-target-content d-flex-column">
-                                                {faq.content.map((item) => (
-                                                    <div>
-                                                        <span className="material-symbols-outlined">check</span>
-                                                        <p key={item.id} className="text">{item.text}</p>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-
-                            <div className="index-services-target index-services-target-2 d-flex-column gap-20">
-                                <video src="https://www.gruasememca.com/media/videos/video-1.webm" className="video-simple" controls></video>
-
-                                <ul className='list-simple margin-bottom-20'>
-                                    {t('homeServicesTagTwo', { returnObjects: true }).map((item) => (
-                                        <li key={item.id}>
-                                            <span className="material-symbols-outlined list-icon-check">check</span>
-                                            <p>{item.text}</p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                </div>
+                <Hero title={t('hero.0.h1')} titleSpan={t('hero.0.h1Span')} />
 
                 <div className="block-container">
                     <section className="block-content">
-                        <ul className="home-beneficts">
-                            {t('homeBeneficts', { returnObjects: true }).map((item) => (
-                                <li key={item.id}>
-                                    <img src={item.img}></img>
-                                    <p className="text-title">{item.title}</p>
-                                    <p className="text">{item.text}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    </section>
-                </div>
-
-                <div className="block-container">
-                    <section className="block-content">
-                        <p>What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        
                     </section>
                 </div>
             </main>
