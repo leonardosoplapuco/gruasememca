6e2f4ab47bbfcb4ce50bcf989f339dc349a5508d
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2024-11-26 17:48:40 -0500

    Actualización 6.4  - leonardosoplapuco

---
+++ src/Components/BlogTargets.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { useTranslation } from 'react-i18next';
+
+function BlogTargets(){
+    // const { t } = useTranslation('chatBot');
+
+    return(
+        <div className='block-container'>
+            <section className='block-content'>
+                <div className='block-title-container'>
+                    <h3>Últimas novedades</h3>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default BlogTargets;
