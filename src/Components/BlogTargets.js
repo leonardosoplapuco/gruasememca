b117adccbe1de10d037b64b5ebfa3054d93396f6
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2024-12-02 19:55:51 -0500

    Actualización 6.8 - leonardosoplapuco

--- src/Components/BlogTargets.js
+++ src/Components/BlogTargets.js
@@ -1,15 +1,31 @@
 import React from 'react';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-function BlogTargets(){
-    // const { t } = useTranslation('chatBot');
+function BlogTargets() {
+    const { t } = useTranslation('blogtargets');
 
-    return(
+    return (
         <div className='block-container'>
             <section className='block-content'>
                 <div className='block-title-container'>
-                    <h3>Últimas novedades</h3>
+                    <h3>{t('title')}</h3>
                 </div>
+
+                <ul className="blog-targets">
+                    {t("list", { returnObjects: true }).map((item) => (
+                        <li key={item.id}>
+                            <a href={item.href || "#"} className="hover-target">
+                                {item.imgSrc && (
+                                    <img src={item.imgSrc} alt={item.imgAlt || "Imagen"} />
+                                )}
+                                <div className="d-flex-column">
+                                    {item.content?.[0]?.title && <p className='text-title'>{item.content[0].title}</p>}
+                                    {item.content?.[0]?.text && <p className='text'>{item.content[0].text}</p>}
+                                </div>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </section>
         </div>
     );
---
+++ src/Components/footer.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+function Footer(){
+    const { t } = useTranslation('footer');
+
+    return(
+        <footer>asddsa</footer>
+    );
+}
+
+export default Footer;
